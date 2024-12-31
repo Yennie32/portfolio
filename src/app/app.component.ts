0544c8e6931e984197c0fad5eaa84e35ba8796af
Your Name <you@example.com>
2024-12-31 17:57:34 +0100

    hero component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from "./hero/hero.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    HeaderComponent
-  ],
+    HeaderComponent,
+    HeroComponent,
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
