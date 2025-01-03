627e76b09fca5ed2ce50d16546d6d95216942970
Your Name <you@example.com>
2025-01-03 15:37:42 +0100

    commit before update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from "./hero/hero.component";
+import { DescriptionComponent } from "./description/description.component";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,7 @@ import { HeroComponent } from "./hero/hero.component";
   imports: [
     HeaderComponent,
     HeroComponent,
+    DescriptionComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
