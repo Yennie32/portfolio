b0cb7fb8a8d0d23b981e31536186345d370fb9ef
Yennie <yennielake@gmail.com>
2025-01-11 17:58:37 +0100

    about-me mobile version to finish

--- src/app/features/contact/pages/contact-page/contact-page.component.ts
+++ src/app/features/contact/pages/contact-page/contact-page.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { PageTitleComponent } from '../../../../shared/components/page-title/page-title.component';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-page',
-  imports: [PageTitleComponent],
+  imports: [PageTitleComponent, ReactiveFormsModule],
+  standalone: true,
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.css',
 })
