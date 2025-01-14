0aa49599c5e4a1c735d7f612db985aa304fc337b
Yennie <yennielake@gmail.com>
2025-01-14 06:33:17 +0100

    image slider : WIP

--- src/app/features/projects/components/carousel/carousel.component.ts
+++ src/app/features/projects/components/carousel/carousel.component.ts
@@ -1,11 +1,68 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Projects {
+  id: number;
+  name: string;
+  role: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-carousel',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.css'
+  styleUrl: './carousel.component.css',
 })
 export class CarouselComponent {
+  selectedProject = 4; // Default active project
 
+  projects: Projects[] = [
+    {
+      id: 1,
+      name: 'Mike',
+      role: 'Web3 Developer',
+      image: '/assets/img/1.png',
+    },
+    {
+      id: 2,
+      name: 'Samite',
+      role: 'Wordpress Developer',
+      image: '/assets/img/2.png',
+    },
+    {
+      id: 3,
+      name: 'Hashi',
+      role: 'Java Developer',
+      image: '/assets/img/3.png',
+    },
+    {
+      id: 4,
+      name: 'Kaity',
+      role: 'Web Developer',
+      image: '/assets/img/4.png',
+    },
+    {
+      id: 5,
+      name: 'Lauren',
+      role: 'PHP Developer',
+      image: '/assets/img/5.png',
+    },
+    {
+      id: 6,
+      name: 'Ryan',
+      role: 'SEO Developer',
+      image: '/assets/img/6.png',
+    },
+    {
+      id: 7,
+      name: 'Dakes',
+      role: 'SQL Developer',
+      image: '/assets/img/7.png',
+    },
+  ];
+
+  selectProject(id: number) {
+    this.selectedProject = id;
+  }
 }
--- src/app/features/projects/pages/project-page/project-page.component.ts
+++ src/app/features/projects/pages/project-page/project-page.component.ts
@@ -1,55 +1,48 @@
 import { Component } from '@angular/core';
 import { PageTitleComponent } from '../../../../shared/components/page-title/page-title.component';
-import { CommonModule, NgFor, NgForOf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { CarouselComponent } from '../../components/carousel/carousel.component';
 
 @Component({
   selector: 'app-project-page',
-  imports: [
-    PageTitleComponent,
-    CommonModule,
-    NgFor,
-    NgForOf,
-    CarouselComponent,
-  ],
+  imports: [PageTitleComponent, CommonModule, CarouselComponent],
   standalone: true,
   templateUrl: './project-page.component.html',
   styleUrl: './project-page.component.css',
 })
 export class ProjectPageComponent {
-  slides = [
-    {
-      title: 'Slide1',
-      image: './assets/img/mock-image.jpeg',
-      infos: 'X coéquipiers, Langage : gnagnagnagna',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi corporis similique consequuntur dolor, beatae deserunt consectetur ad repellat eos reprehenderit provident expedita excepturi rem possimus fugiat culpa modi facilis voluptatem hic libero. Fugiat voluptatum iure, quas possimus perferendis delectus eaque dignissimos accusamus debitis repudiandae repellendus esse quisquam assumenda necessitatibus ips',
-      github: 'github',
-    },
-    {
-      title: 'Slide2',
-      image: './assets/img/mock-image2.jpeg',
-      infos: 'X coéquipiers, Langage : gnagnagnagna',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi corporis similique consequuntur dolor, beatae deserunt consectetur ad repellat eos reprehenderit provident expedita excepturi rem possimus fugiat culpa modi facilis voluptatem hic libero. Fugiat voluptatum iure, quas possimus perferendis delectus eaque dignissimos accusamus debitis repudiandae repellendus esse quisquam assumenda necessitatibus ips',
-      github: 'github',
-    },
-    {
-      title: 'Slide3',
-      image: './assets/img/mock-image3.jpeg',
-      infos: 'X coéquipiers, Langage : gnagnagnagna',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi corporis similique consequuntur dolor, beatae deserunt consectetur ad repellat eos reprehenderit provident expedita excepturi rem possimus fugiat culpa modi facilis voluptatem hic libero. Fugiat voluptatum iure, quas possimus perferendis delectus eaque dignissimos accusamus debitis repudiandae repellendus esse quisquam assumenda necessitatibus ips',
-      github: 'github',
-    },
-  ];
-  currentSlide = 0;
-
-  nextSlide() {
-    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
-  }
-  previousSlide() {
-    this.currentSlide =
-      (this.currentSlide - 1 + this.slides.length) % this.slides.length;
-  }
+  // slides = [
+  //   {
+  //     title: 'Slide1',
+  //     image: './assets/img/mock-image.jpeg',
+  //     infos: 'X coéquipiers, Langage : gnagnagnagna',
+  //     description:
+  //       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi corporis similique consequuntur dolor, beatae deserunt consectetur ad repellat eos reprehenderit provident expedita excepturi rem possimus fugiat culpa modi facilis voluptatem hic libero. Fugiat voluptatum iure, quas possimus perferendis delectus eaque dignissimos accusamus debitis repudiandae repellendus esse quisquam assumenda necessitatibus ips',
+  //     github: 'github',
+  //   },
+  //   {
+  //     title: 'Slide2',
+  //     image: './assets/img/mock-image2.jpeg',
+  //     infos: 'X coéquipiers, Langage : gnagnagnagna',
+  //     description:
+  //       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi corporis similique consequuntur dolor, beatae deserunt consectetur ad repellat eos reprehenderit provident expedita excepturi rem possimus fugiat culpa modi facilis voluptatem hic libero. Fugiat voluptatum iure, quas possimus perferendis delectus eaque dignissimos accusamus debitis repudiandae repellendus esse quisquam assumenda necessitatibus ips',
+  //     github: 'github',
+  //   },
+  //   {
+  //     title: 'Slide3',
+  //     image: './assets/img/mock-image3.jpeg',
+  //     infos: 'X coéquipiers, Langage : gnagnagnagna',
+  //     description:
+  //       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi corporis similique consequuntur dolor, beatae deserunt consectetur ad repellat eos reprehenderit provident expedita excepturi rem possimus fugiat culpa modi facilis voluptatem hic libero. Fugiat voluptatum iure, quas possimus perferendis delectus eaque dignissimos accusamus debitis repudiandae repellendus esse quisquam assumenda necessitatibus ips',
+  //     github: 'github',
+  //   },
+  // ];
+  // currentSlide = 0;
+  // nextSlide() {
+  //   this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+  // }
+  // previousSlide() {
+  //   this.currentSlide =
+  //     (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+  // }
 }
