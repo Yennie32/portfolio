57cb5c8c27b520a112113663ae92b8c8c339dcd4
Yennie <yennielake@gmail.com>
2025-01-15 18:09:43 +0100

    updating from main

--- src/app/features/projects/components/carousel/carousel.component.ts
+++ src/app/features/projects/components/carousel/carousel.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CarouselService, Project } from '../../service/carousel.service';
 
 @Component({
